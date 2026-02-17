ad107ab38ac6ba2d545d39e8306f7f3c5f11fbbb
Katar1ne <katarineleite5@gmail.com>
2026-02-17 12:10:38 -0300

    fix: altera cor do botão inserir tag

--- packages/frontend/web/src/components/editor/styles.ts
+++ packages/frontend/web/src/components/editor/styles.ts
@@ -2,6 +2,15 @@ import styled from 'styled-components'
 export const AddTagButton = styled.div`
   button {
     font-size: 12px;
-    gap: 4px;
+    gap: 6px;
+    background-color: #2978b5;
+    color: #ffffff;
+    font-weight: bold;
+    border-radius: 5px;
+    border: none;
+    
+    &:hover {
+      background-color: #1f5f8b;
+    }
   }
 `
