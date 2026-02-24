e1466877bfbfaaafd13fd20b626ea04f5c1fbc8f
Igor César <66170076+igorcesar-dev@users.noreply.github.com>
2026-02-23 21:57:34 -0300

    refactor: ajustes no layout e melhorias nas validações

--- packages/frontend/web/src/components/infoStep/index.tsx
+++ packages/frontend/web/src/components/infoStep/index.tsx
@@ -1,13 +1,15 @@
-import { Alert, FileChooser } from '@components'
+import { Alert, Button, FileChooser } from '@components'
 import { IDataSheet } from '@utils'
 import { FaRegFileExcel } from 'react-icons/fa'
-import { FiInfo } from 'react-icons/fi'
+import { FiDownload, FiInfo } from 'react-icons/fi'
 
 import { SheetTable, SheetTableWrapper } from './styles'
 
 interface Props {
   onUpload: (file: FormData) => void
   onRemove: () => void
+  onDownload: () => void
+  loading: boolean
   dataSheet: IDataSheet[]
   examples: string[][]
 }
@@ -15,23 +17,13 @@ interface Props {
 export const InfoStep: React.FC<Props> = ({
   onUpload,
   onRemove,
+  onDownload,
+  loading,
   dataSheet,
   examples
 }) => {
   return (
     <>
-      <Alert icon={FiInfo} marginBottom="sm">
-        Faça o upload da planilha que você baixou na tela anterior.
-      </Alert>
-      <FileChooser
-        title="Arraste e solte a planilha aqui"
-        onUpload={onUpload}
-        onRemove={onRemove}
-        icon={FaRegFileExcel}
-        type="spreadsheet"
-        height="240px"
-        marginBottom="md"
-      />
       <Alert card type="warning" marginBottom="md">
         <p>
           <b>Atenção!</b>
@@ -41,7 +33,7 @@ export const InfoStep: React.FC<Props> = ({
           seguintes regras.
         </p>
       </Alert>
-      <Alert icon={FiInfo} marginBottom="sm">
+      <Alert icon={FiInfo} marginBottom="md">
         A planilha <b>deve conter as mesmas colunas</b> exibidas a seguir:
       </Alert>
       <SheetTableWrapper>
@@ -66,6 +58,28 @@ export const InfoStep: React.FC<Props> = ({
           </tbody>
         </SheetTable>
       </SheetTableWrapper>
+      <Button
+        onClick={onDownload}
+        marginBottom="md"
+        outline
+        color="secondary"
+        loading={loading}
+      >
+        <FiDownload size={20} />
+        <span>Baixar Planilha de Exemplo</span>
+      </Button>
+      <Alert icon={FiInfo} marginBottom="sm">
+        Faça o upload da planilha que você baixou como modelo.
+      </Alert>
+      <FileChooser
+        title="Arraste e solte a planilha aqui"
+        onUpload={onUpload}
+        onRemove={onRemove}
+        icon={FaRegFileExcel}
+        type="spreadsheet"
+        height="240px"
+        marginBottom="md"
+      />
     </>
   )
 }
